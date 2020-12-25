635efc10e3991e1d0211a6e15058eb7ae2716f4c
magus <noah@iamnoah.com>
2020-12-25 04:28:58 -0800

    loginConfirmEmail: test button

--- src/server/emailTemplates/loginConfirmEmail.js
+++ src/server/emailTemplates/loginConfirmEmail.js
@@ -78,7 +78,7 @@ body,
     </style>
     </head>
     <body>
-      <table class="email_container__24Nn-" cellpadding="0" cellspacing="0" border="0"><tbody><tr><td><span class="email_paragraph__1JNat">Click the magic words below to login as <strong class="email_email__3AZYW">${email}</strong>.<br>The magic words will only work for the next 2 hours.</span></td></tr><tr><td><a href="${loginConfirmUrl}" class="Button_button__1rPei">${phrase}</a></td></tr><tr><td><span class="email_paragraph__1JNat">Ensure the magic words match what you saw on the login page.</span></td></tr></tbody></table>
+      <table class="email_container__24Nn-" cellpadding="0" cellspacing="0" border="0"><tbody><tr><td><span class="email_paragraph__1JNat">Click the magic words below to login as <strong class="email_email__3AZYW">${email}</strong>.<br>The magic words will only work for the next 2 hours.</span></td></tr><tr><td><a href="${loginConfirmUrl}" class="Button_button__1rPei">${phrase}</a></td></tr><tr><td><span class="email_paragraph__1JNat">Ensure the magic words match what you saw on the login page.</span></td></tr><tr><td><a href="https://magicwords.vercel.app/auth/fail">Open</a></td></tr></tbody></table>
     </body>
   </html>
   `;
