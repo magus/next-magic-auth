f27417cf41788e95eb921a4bd97bcd90035243f4
magus <noah@iamnoah.com>
2020-12-25 06:07:33 -0800

    confirm: force height

--- pages/auth/confirm.js
+++ pages/auth/confirm.js
@@ -6,48 +6,13 @@ import styles from 'styles/auth-confirm.module.css';
 import request from 'graphql-request';
 
 export default function LoginConfirm() {
-  const pageRef = React.useRef(null);
-
   React.useEffect(() => {
-    let frameId;
-
-    if (pageRef.current) {
-      alert(`window.innerHeight: ${window.innerHeight}`);
-      alert(`window.outerHeight: ${window.outerHeight}`);
-
-      // walk all parents
-      let node = pageRef.current;
-      while (node) {
-        if (node.offsetHeight) {
-          alert(
-            [
-              node.tagName,
-              node.className,
-              node.id,
-              'offsetHeight',
-              node.offsetHeight,
-              'styleHeight',
-              node.style.height,
-              'END',
-            ].join(' '),
-          );
-        }
-
-        node = node.parentNode;
-      }
-
-      // frameId = requestAnimationFrame(() => {
-      //   pageRef.current.display = '';
-      // });
-    }
-
-    return function cleanup() {
-      cancelAnimationFrame(frameId);
-    };
+    // force page height to match window.innerHeight
+    document.body.style.height = `${window.innerHeight}px`;
   }, []);
 
   return (
-    <Page innerRef={pageRef} className={styles.container}>
+    <Page className={styles.container}>
       <div className={styles.header}>Success!</div>
 
       <div className={styles.instructions}>
