7eea9292ef47f7143b147bf6b81040aa5ffe9436
magus <noah@iamnoah.com>
2020-12-25 03:10:58 -0800

    loginConfirmEmail style in head tag

--- src/server/emailTemplates/loginConfirmEmail.js
+++ src/server/emailTemplates/loginConfirmEmail.js
@@ -4,83 +4,83 @@ export default function loginConfirmEmail({ email, loginConfirmUrl, phrase }) {
   <html>
     <head>
       <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      <style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  color: rgba(26, 9, 13, 1.0);
+  font-size: 16px;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  -moz-box-sizing: border-box;
+        box-sizing: border-box;
+
+  /*Reset's every elements apperance*/
+  background: none repeat scroll 0 0 transparent;
+  border: medium none;
+  border-spacing: 0;
+  list-style: none outside none;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  text-decoration: none;
+  text-indent: 0;
+  color: rgba(26, 9, 13, 1.0);
+  font-size: 16px;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-weight: normal;
+  line-height: 1.42rem;
+}
+
+html,
+body,
+#__next {
+  height: 100%;
+}
+
+.Button_button__1rPei {
+  width: 100%;
+  height: 48px;
+  -moz-border-radius: 8px;
+        border-radius: 8px;
+  background-color: rgba(249, 109, 16, 1.0);
+  display: -webkit-flex;
+  display: -moz-box;
+  display: flex;
+  -webkit-justify-content: center;
+      -moz-box-pack: center;
+          justify-content: center;
+  line-height: 48px;
+  color: rgba(255, 255, 255, 0.9);
+  font-weight: 700;
+}
+
+.email_container__24Nn- {
+  max-width: 450px;
+  text-align: left;
+}
+
+.email_email__3AZYW {
+  font-weight: 800;
+}
+
+.email_paragraph__1JNat {
+  display: inline-block;
+  margin: 16px 0;
+}
+    </style>
     </head>
     <body>
-      <style>
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    color: rgba(26, 9, 13, 1.0);
-    font-size: 16px;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  * {
-    -moz-box-sizing: border-box;
-         box-sizing: border-box;
-
-    /*Reset's every elements apperance*/
-    background: none repeat scroll 0 0 transparent;
-    border: medium none;
-    border-spacing: 0;
-    list-style: none outside none;
-    margin: 0;
-    padding: 0;
-    text-align: left;
-    text-decoration: none;
-    text-indent: 0;
-    color: rgba(26, 9, 13, 1.0);
-    font-size: 16px;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    font-weight: normal;
-    line-height: 1.42rem;
-  }
-
-  html,
-  body,
-  #__next {
-    height: 100%;
-  }
-
-  .Button_button__1rPei {
-    width: 100%;
-    height: 48px;
-    -moz-border-radius: 8px;
-         border-radius: 8px;
-    background-color: rgba(249, 109, 16, 1.0);
-    display: -webkit-flex;
-    display: -moz-box;
-    display: flex;
-    -webkit-justify-content: center;
-       -moz-box-pack: center;
-            justify-content: center;
-    line-height: 48px;
-    color: rgba(255, 255, 255, 0.9);
-    font-weight: 700;
-  }
-
-  .email_container__24Nn- {
-    max-width: 450px;
-    text-align: left;
-  }
-
-  .email_email__3AZYW {
-    font-weight: 800;
-  }
-
-  .email_paragraph__1JNat {
-    display: inline-block;
-    margin: 16px 0;
-  }
-      </style>
       <table class="email_container__24Nn-" cellpadding="0" cellspacing="0" border="0"><tbody><tr><td><span class="email_paragraph__1JNat">Click the magic words below to login as <b class="email_email__3AZYW">${email}</b>.<br>The magic words will only work for the next 2 hours.</span></td></tr><tr><td><a href="${loginConfirmUrl}" style=""><button class="Button_button__1rPei">${phrase}</button></a></td></tr><tr><td><span class="email_paragraph__1JNat">Ensure the magic words match what you saw on the login page.</span></td></tr></tbody></table>
     </body>
   </html>
