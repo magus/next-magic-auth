b8baab731e7e91309c7a86289f73b168398b263e
magus <noah@iamnoah.com>
2020-12-25 02:47:29 -0800

    auth confirm styles

--- pages/auth/confirm.js
+++ pages/auth/confirm.js
@@ -1,7 +1,15 @@
+import Page from 'components/Page';
+
+import styles from 'styles/auth-confirm.module.css';
+
 export default function LoginConfirm() {
   return (
-    <div>
-      <h1> Login confirmed! </h1>
-    </div>
+    <Page className={styles.container}>
+      <div className={styles.header}>Login confirmed!</div>
+
+      <div className={styles.instructions}>
+        Go back to the page, you should now be logged in.
+      </div>
+    </Page>
   );
 }
