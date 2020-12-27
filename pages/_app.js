ae865668c2dfa0037930b061c00feea1a6c2cd7c
magus <noah@iamnoah.com>
2020-12-27 02:06:40 -0800

    components/TimeAgo

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { IntlProvider } from 'react-intl';
 
 import { AuthProvider } from 'src/components/AuthProvider';
 import ApolloProvider from 'src/components/ApolloProvider';
@@ -8,22 +9,24 @@ import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <ApolloProvider>
-        <ModalContextProvider>
-          <Head>
-            <title>Magic</title>
-            <meta
-              name="viewport"
-              content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no"
-              key="viewport"
-            />
-          </Head>
+    <IntlProvider locale="en" defaultLocale="en">
+      <AuthProvider>
+        <ApolloProvider>
+          <ModalContextProvider>
+            <Head>
+              <title>Magic</title>
+              <meta
+                name="viewport"
+                content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no"
+                key="viewport"
+              />
+            </Head>
 
-          <Component {...pageProps} />
-        </ModalContextProvider>
-      </ApolloProvider>
-    </AuthProvider>
+            <Component {...pageProps} />
+          </ModalContextProvider>
+        </ApolloProvider>
+      </AuthProvider>
+    </IntlProvider>
   );
 }
 
---
+++ src/components/TimeAgo.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { FormattedRelativeTime } from 'react-intl';
+
+function getTimeAgoData(date) {
+  const now = Date.now();
+  const secondsAgo = (date.getTime() - now) / 1000;
+  const isPast = secondsAgo < 0;
+  const isFuture = secondsAgo > 0;
+
+  return { now, secondsAgo, isPast, isFuture };
+}
+
+export default function TimeAgo(props) {
+  const date = props.date instanceof Date ? props.date : new Date(props.date);
+
+  return (
+    <FormattedRelativeTime
+      value={getTimeAgoData(date).secondsAgo}
+      numeric="auto"
+      style="long"
+      updateIntervalInSeconds={1}
+    >
+      {(formattedDate) => {
+        console.debug('[TimeAgo]', { formattedDate });
+
+        if (typeof props.children === 'function') {
+          return props.children(formattedDate, getTimeAgoData(date));
+        }
+
+        return formattedDate;
+      }}
+    </FormattedRelativeTime>
+  );
+}
