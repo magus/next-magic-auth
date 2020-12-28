583f456fe0c20a2245d56f35993e34b17e3e7040
magus <noah@iamnoah.com>
2020-12-28 04:19:21 -0800

    _app: meta separator , not ;

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
               <title>Magic</title>
               <meta
                 name="viewport"
-                content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
                 key="viewport"
               />
             </Head>
