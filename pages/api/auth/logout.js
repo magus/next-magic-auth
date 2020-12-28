5c702d0a82422c92d115cb4f85373677728dac31
magus <noah@iamnoah.com>
2020-12-27 20:40:48 -0800

    api/auth/logout: delete loginToken

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,8 +1,20 @@
+import gql from 'graphql-tag';
+
 import auth from 'src/server/auth';
+import graphql from 'src/server/graphql';
 
 export default async function loginRefresh(req, res) {
   try {
     auth.clearCookies(res);
+
+    const loginRequestId = auth.getLoginRequest(req);
+    if (loginRequestId) {
+      // delete login request (loginToken)
+      // this will also cascade and delete refreshTokens
+      await graphql.query(deleteLoginToken, {
+        variables: { id: loginRequestId },
+      });
+    }
 
     return res.status(200).json({ error: false });
   } catch (e) {
@@ -13,3 +25,11 @@ export default async function loginRefresh(req, res) {
       .json({ error: true, message: e.message, stack: e.stack.split('\n') });
   }
 }
+
+const deleteLoginToken = gql`
+  mutation MyMutation($id: uuid!) {
+    delete_loginToken_by_pk(id: $id) {
+      id
+    }
+  }
+`;
