1c20cd5765e03e8ae04c6a268346c49eae432c97
magus <noah@iamnoah.com>
2020-12-29 02:01:45 -0800

    Button: simple

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,20 @@ import Link from 'next/link';
 
 import styles from 'styles/Button.module.css';
 
-export default function Button({ className, children, href, ...restProps }) {
-  if (href) {
+export default function Button({
+  className,
+  children,
+  href,
+  simple,
+  ...restProps
+}) {
+  if (simple) {
+    return (
+      <button className={[styles.simple, className].join(' ')} {...restProps}>
+        {children}
+      </button>
+    );
+  } else if (href) {
     return (
       <Link href={href}>
         <a
