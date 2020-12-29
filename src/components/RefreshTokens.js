f5ce337700646957752a7a993d2df0796ea79ac1
magus <noah@iamnoah.com>
2020-12-29 02:02:06 -0800

    RefreshTokens: delete sessions

--- src/components/RefreshTokens.js
+++ src/components/RefreshTokens.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 
 import { useAuth } from 'src/components/AuthProvider';
-
+import Button from 'src/components/Button';
 import Table from 'src/components/Table';
 import TimeAgo from 'src/components/TimeAgo';
-import graphql from 'src/client/graphql/queries';
+
+import headers from 'src/shared/headers';
+import roles from 'src/shared/roles';
 
 export default function RefreshTokens({ loading, refreshTokens }) {
   const auth = useAuth();
+  const [deletingSessions, set_deletingSessions] = React.useState({});
 
   const header = `Active sessions${
     loading ? '' : ` (${refreshTokens.length})`
@@ -15,31 +20,103 @@ export default function RefreshTokens({ loading, refreshTokens }) {
   const columns = ['', 'Device', 'IP', 'Last active', 'Details'];
   const loadingWidths = [0, 200, 100, 100, 200];
 
+  // mark session as 'deleting'
+  async function handleDeleteSession(id) {
+    set_deletingSessions({ ...deletingSessions, [id]: true });
+  }
+  // unmark session as 'deleting'
+  async function handleDeleteSessionError(id) {
+    const new_deletingSessions = { ...deletingSessions };
+    delete new_deletingSessions[id];
+    set_deletingSessions(new_deletingSessions);
+  }
+
   return (
     <Table {...{ header, columns, loading, loadingWidths }}>
       {refreshTokens.map((rt) => {
         return (
           <tr key={rt.id}>
-            <td>{auth.loginRequestId === rt.id ? '🎉' : ' '}</td>
-
-            <td>{rt.userAgent}</td>
-
-            <td>{rt.ip}</td>
-
             <td>
-              <TimeAgo date={rt.lastActive} />
+              {auth.loginRequestId === rt.id ? (
+                '🎉'
+              ) : (
+                <DeleteSession
+                  id={rt.id}
+                  onDelete={handleDeleteSession}
+                  onError={handleDeleteSessionError}
+                />
+              )}
             </td>
 
-            <td>
-              <TimeAgo date={rt.created}>
-                {(formattedDate, timeAgoData) => {
-                  return `Created ${formattedDate}`;
-                }}
-              </TimeAgo>
-            </td>
+            {deletingSessions[rt.id] ? (
+              <td colSpan="4">Logging out session...</td>
+            ) : (
+              <React.Fragment>
+                <td>{rt.userAgent}</td>
+
+                <td>{rt.ip}</td>
+
+                <td>
+                  <TimeAgo date={rt.lastActive} />
+                </td>
+
+                <td>
+                  <TimeAgo date={rt.created}>
+                    {(formattedDate, timeAgoData) => {
+                      return `Created ${formattedDate}`;
+                    }}
+                  </TimeAgo>
+                </td>
+              </React.Fragment>
+            )}
           </tr>
         );
       })}
     </Table>
   );
 }
+
+function DeleteSession({ id, onDelete, onError }) {
+  const [deleteSession, { data, loading, error }] = useMutation(
+    deleteLoginToken,
+    {
+      variables: { id },
+      context: {
+        headers: {
+          [headers.role]: roles.self,
+        },
+      },
+    },
+  );
+
+  React.useEffect(() => {
+    if (error) {
+      onError(id);
+    }
+  }, [error]);
+
+  async function handleDeleteSession() {
+    onDelete(id);
+    deleteSession();
+    // // simulate error
+    // setTimeout(() => {
+    //   onError(id);
+    // }, 2000);
+  }
+
+  const title = loading ? 'Logout this session' : 'Logging out session';
+
+  return (
+    <Button simple title={title} onClick={handleDeleteSession}>
+      {loading ? '⏳' : '❌'}
+    </Button>
+  );
+}
+
+const deleteLoginToken = gql`
+  mutation DeleteLoginToken($id: uuid!) {
+    delete_loginToken_by_pk(id: $id) {
+      id
+    }
+  }
+`;
