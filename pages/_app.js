d6418272920eab90a60735eecbe5d4fc558ccd46
magus <noah@iamnoah.com>
2021-01-09 20:32:23 -0800

    _app: favicon

--- pages/_app.js
+++ pages/_app.js
@@ -67,6 +67,11 @@ function AppShell({ children, Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
           key="viewport"
         />
+        <link
+          key="favicon"
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🪄</text></svg>"
+        />
       </Head>
 
       {children}
