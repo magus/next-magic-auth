5fc93ba0ac04de2392bb8c430144da83f17c6b18
magus <noah@iamnoah.com>
2021-01-15 21:45:36 -0800

    Game page

---
+++ pages/game.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import Page from 'src/components/Page';
+import Button from 'src/components/Button';
+import { useAuth } from 'src/components/AuthProvider';
+
+import styles from 'styles/Home.module.css';
+
+GamePage.title = null;
+
+const ModuleLoaders = [
+  {
+    name: 'luxon',
+    load: () => import('luxon'),
+  },
+];
+
+export default function GamePage(props) {
+  const auth = useAuth();
+
+  const [numberLoaded, set_numberLoaded] = React.useState(0);
+  const [message, set_message] = React.useState('Loading');
+
+  React.useEffect(() => {
+    console.debug('[Game]', 'mount');
+    ModuleLoaders.forEach((moduleLoader) => {
+      set_message(`Loading ${moduleLoader.name}`);
+      moduleLoader.load().then((...args) => {
+        set_numberLoaded((n) => n + 1);
+      });
+    });
+  }, []);
+
+  // console.debug('[Game]', { auth });
+  return (
+    <Page className={styles.container}>
+      <div className={styles.containerContent}>
+        <div>{message}</div>
+        <div>{`${numberLoaded} / ${ModuleLoaders.length}`}</div>
+      </div>
+    </Page>
+  );
+}
