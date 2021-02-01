a5a5e00dabe08c89602888ed201ab2f29982268a
magus <noah@iamnoah.com>
2021-02-01 02:13:22 -0800

    Physics: velocity verlet

--- src/game/Physics.js
+++ src/game/Physics.js
@@ -9,12 +9,11 @@ export function processGravity(player, deltaTime) {
     // convert deltaTime (ms) to seconds for our math
     const time = deltaTime / 1000;
 
-    // move player based on velocity
-    player.y = player.y + player.velocity.y * time;
+    // calculate new position based on velocity
+    player.y += p_vg(player.velocity.y, time);
 
-    // calculate new velocity over delta time and ensure velocity does not go below zero
-    const velocity_y = v_va(player.velocity.y, g)(time);
-    player.velocity.y = velocity_y;
+    // calculate new velocity based on gravity (v_g)
+    player.velocity.y += v_g(time);
 
     // when player touches ground, set velocity to 0
     if (player.y <= groundLevel(player.x, player.y, player.z)) {
@@ -33,9 +32,11 @@ const g = -9.8 * 2;
 // for example, there may be a spot on the zone map where the ground is not 0 and is instead 500 or -500
 const groundLevel = (x, y, z) => 0;
 
-// velocity as function of time
-const v_va = (v, a) => (t) => v + a * t;
+// change in velocity as function of time
+const v_a = (a) => (t) => a * t;
+const v_g = v_a(g);
 
-// displacement as a function of time and initial velocity
-// d = (v * t) + (0.5 * a * t^2)
-// const d_va = (v, a) => (t) => v * t + 0.5 * g * Math.pow(t, 2);
+// displacement (change in position) as a function of time (using acceleration and velocity)
+// i.e. d = (v * t) + (0.5 * a * t^2)
+const p_va = (v, a) => (t) => v * t + 0.5 * a * Math.pow(t, 2);
+const p_vg = (v, t) => p_va(v, g)(t);
